65de2f77a5d73ec7b055ce612f2dd8a78321b579
Stu Field <stu.g.field@gmail.com>
2024-10-28 08:50:10 -0600

    Pull out calc* functions from package
    - now they live in another package
    - now use calc_univariate()

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,5 +1,4 @@
 # Warning will go away with next `roxygen2` version
 list(
-  rd_family_title = list(calc = "The `calc.*()` family: ",
-                         fit  = "The `fit*()` family: ")
+  rd_family_title = list(fit  = "The `fit*()` family: ")
 )
